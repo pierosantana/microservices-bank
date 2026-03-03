c6fea34f4314b1446dea4a841c5863aaebe42529
pierosantana <piero.santanalarosa@gmail.com>
2026-03-03 19:17:21 +0100

    feat: agregar base del microservicio mcc-customer-service

---
+++ mcc-customer-service/src/main/java/com/psantana/mcc_customer_service/MccCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.psantana.mcc_customer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccCustomerServiceApplication.class, args);
+	}
+
+}
---
+++ mcc-customer-service/src/test/java/com/psantana/mcc_customer_service/MccCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psantana.mcc_customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
