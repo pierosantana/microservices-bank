c6d5ddb0417bcc4bf0078e88ea46e4cc9d3bbb15
pierosantana <piero.santanalarosa@gmail.com>
2026-03-05 17:17:20 +0100

    chore: commit inicial

---
+++ mcc-credit-disbursement-service/src/main/java/com/psantana/mcc_credit_disbursement_service/MccCreditDisbursementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.psantana.mcc_credit_disbursement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccCreditDisbursementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccCreditDisbursementServiceApplication.class, args);
+	}
+
+}
---
+++ mcc-credit-disbursement-service/src/test/java/com/psantana/mcc_credit_disbursement_service/MccCreditDisbursementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psantana.mcc_credit_disbursement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccCreditDisbursementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
