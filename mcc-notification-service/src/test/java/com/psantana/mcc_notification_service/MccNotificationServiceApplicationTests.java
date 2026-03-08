1dcaa019f03030ac154152e28089ce47d914ba69
pierosantana <piero.santanalarosa@gmail.com>
2026-03-08 17:30:42 +0100

    chore: commit inicial mcc-notification-service

---
+++ mcc-notification-service/src/test/java/com/psantana/mcc_notification_service/MccNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psantana.mcc_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
