ef05056b8fae1a5a95123769000a974f5e2b88d3
pierosantana <piero.santanalarosa@gmail.com>
2026-03-10 13:31:54 +0100

    chore(mcc-gateway-service): commit inicial

---
+++ mcc-gateway-service/src/test/java/com/psantana/mcc_gateway_service/MccGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psantana.mcc_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
